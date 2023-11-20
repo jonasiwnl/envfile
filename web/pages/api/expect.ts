1a7184dc29129516e1b4d99f4cac34c88dbed54c
arael <jonasg@umich.edu>
2023-11-20 16:34:19 -0500

    flesh out server-side with redis

--- web/pages/api/expect.ts
+++ web/pages/api/expect.ts
@@ -1,12 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "redis";
+
+const redis = createClient({
+    url: process.env.REDIS,
+});
+
+const keywords = [
+    "cat",
+    "dog",
+    "apple",
+    "banana",
+    "orange",
+    "pear",
+    "bird",
+    "fish",
+    "turtle",
+    "snake",
+    "lizard",
+    "frog",
+    "pear",
+];
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
-    // Generate keyword
-    // Store keyword -> IP in KV store
-    // Send keyword to user
+    if (req.method !== "GET") return res.status(405).end();
 
-    res.status(200).json({ keyword: "cat", ok: true });
+    await redis.connect();
+
+    // TODO check if keyword is already in redis
+    const keyword = keywords[Math.floor(Math.random() * keywords.length)];
+    const ok = true;
+
+    redis.set(keyword, req.socket.remoteAddress || "");
+
+    void redis.disconnect();
+    res.status(200).json({ keyword, ok });
 }
--- web/pages/api/send.ts
+++ web/pages/api/send.ts
@@ -1,10 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "redis";
+
+const redis = createClient({
+    url: process.env.REDIS,
+});
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
-    // Lookup keyword -> IP in KV store
-    // Send IP to user
-    res.status(200).json({ ip: "0.0.0.0:8080", ok: true });
+    if (req.method !== "GET") return res.status(405).end();
+
+    await redis.connect();
+    const { key } = req.query;
+
+    if (typeof key !== "string") return res.status(400).end();
+
+    const ip = await redis.get(key);
+    const ok = ip != null && ip !== "" && ip !== undefined;
+    // TODO what if key doesn't exist
+
+    void redis.disconnect();
+    res.status(200).json({ ip, ok });
 }
